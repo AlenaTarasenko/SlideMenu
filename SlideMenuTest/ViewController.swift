43e54b676264d39ea249db3557fd1fe2c4ba703e
Alena Tarasenko <tarasenkoalena256@gmail.com>
2019-07-13 16:38:27 +0700

    Initial Commit

---
+++ SlideMenuTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenuTest
+//
+//  Created by Alena Tarasenko on 13/07/2019.
+//  Copyright © 2019 Alena Tarasenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
